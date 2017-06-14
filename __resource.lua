efa41b4a8236b3b9dd3994b46d179ce78f8f1150
samuelds <samuelds.pro@gmail.com>
2017-06-14 17:49:25 +0200

    Add AddButton

--- __resource.lua
+++ __resource.lua
@@ -1,6 +1,6 @@
 -- @Date:   2017-06-11T09:31:20+02:00
 -- @Project: FiveM Tools
--- @Last modified time: 2017-06-14T15:04:25+02:00
+-- @Last modified time: 2017-06-14T17:49:21+02:00
 -- @License: GNU General Public License v3.0
 
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
@@ -28,5 +28,6 @@ exports {
   "Current",
   "Freeze",
   "MenuGenerator",
+  "AddButton",
 
 }
