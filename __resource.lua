090cee7153f262aac84455d9b93313e031fb7141
samuelds <samuelds.pro@gmail.com>
2017-06-15 14:10:24 +0200

    Update 0.10

--- __resource.lua
+++ __resource.lua
@@ -1,6 +1,6 @@
 -- @Date:   2017-06-11T09:31:20+02:00
 -- @Project: FiveM Tools
--- @Last modified time: 2017-06-15T12:46:20+02:00
+-- @Last modified time: 2017-06-15T14:10:18+02:00
 -- @License: GNU General Public License v3.0
 
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
@@ -29,6 +29,7 @@ exports {
   "Current",
   "Freeze",
   "AddButton",
+  "CleanButton",
   "Generator",
 
 }
